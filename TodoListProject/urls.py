7e82161e7b7c5c7741a205e74aa2cda9daea32e1
major <majorofffpavel@gmail.com>
2022-02-04 18:06:39 +0300

    add path and static root settings to urls.py

--- TodoListProject/urls.py
+++ TodoListProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from TodoAPP import views
+from TodoListProject import views
 from django.conf.urls.static import static
 from TodoListProject import settings
 
@@ -25,4 +25,15 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('login/', views.loginuser, name='loginuser'),
+
+    # TodoList
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedview, name='completedview'),
+    path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
+
+urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
