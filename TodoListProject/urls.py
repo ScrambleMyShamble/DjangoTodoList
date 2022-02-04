5036c3f414cdd7c81348ae38c682f54dfaed31ea
major <majorofffpavel@gmail.com>
2022-02-04 21:52:17 +0300

    change urls.py

--- TodoListProject/urls.py
+++ TodoListProject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from TodoListProject import views
+from TodoListProject import TodoAPP
 from django.conf.urls.static import static
-from TodoListProject import settings
+from TodoListProject.TodoListProject import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
